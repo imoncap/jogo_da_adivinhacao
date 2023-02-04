aa6047628572132dc62356812e9be7d603c30b85
Igor <imoncap.dev@gmail.com>
2023-02-04 18:36:18 -0300

    Adicionado nível de dificuldade ao jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,7 +5,32 @@ print('Bem vindos(as) ao jogo da adivinhação!')
 print('-'*50)
 
 numero_secreto = random.randrange(1, 101)
-tentativas = 3
+tentativas = 0
+
+print('Escolha um nível de dificuldade:')
+print('(1) Fácil\n(2) Médio\n(3) Difícil\n')
+
+while (tentativas == 0):
+    nivel = int(input('>>> '))
+    if (nivel == 1):
+        print('-' * 50)
+        print('Nível FÁCIL selecionado.\nVocê tem 10 tentativas!')
+        print('-' * 50)
+        tentativas = 10
+    elif (nivel == 2):
+        print('-' * 50)
+        print('Nível MÉDIO selecionado.\nVocê tem 5 tentativas!')
+        print('-' * 50)
+        tentativas = 5
+    elif (nivel == 3):
+        print('-' * 50)
+        print('Nível DIFÍCIL selecionado.\nVocê tem 3 tentativas!')
+        print('-' * 50)
+        tentativas = 3
+    else:
+        print('-' * 50)
+        print('Digite um valor entre 1 e 3.')
+        print('-' * 50)
 
 for rodada in range (1, tentativas + 1, 1):
     print(f'Rodada {rodada} de {tentativas}')
