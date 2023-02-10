2ab30d972d0cb4ac7f09f152f9f8ea5adcf24476
Igor <imoncap.dev@gmail.com>
2023-02-10 10:20:00 -0300

    Implementação da quantidade de tentativas.

--- forca.py
+++ forca.py
@@ -3,26 +3,35 @@ def jogar():
     print('Bem vindo(a) ao jogo da forca!')
     print('-'*50)
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    erros = 0
+
     print(letras_acertadas)
 
     while (not enforcou and not acertou):
         chute = input("Digite uma letra.\n>>> ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
+        if (chute in palavra_secreta):
+            index = 0 #Index = Posição
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
-        index = 0 #Index = Posição
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
-
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
-
+    if(acertou):
+        print("\nVocê acertou a palavra!\n")
+    else:
+        print("\nTentativas esgotadas. Você não conseguiu acertar a palavra :(\n")
     print("Fim de jogo.")
 
 if (__name__ == "__main__"):
